2b85000d2a934a783319b818e436b9cd7b7d4a89
Snehasish Parida <snehasish.parida016@gmail.com>
2025-12-14 12:06:14 +0530

    2nd commit

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
-const API_KEY = "AIzaSyBev4CQwuBsH3nJ9dYnKbTbWI0c0gQRPdA";
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+
 
 const ai=new GoogleGenAI({
     apiKey: API_KEY,
